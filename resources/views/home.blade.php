e7e29a22c10d4f7838242adf6a741d0f3faacb40
SatriaHilmi <vanpers34@gmail.com>
2026-02-26 14:47:17 +0700

    feat: add mobile menu & responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,17 +8,33 @@
         {{-- Overlay --}}
         <div class="absolute inset-0 bg-black/50"></div>
         <div class="relative text-center gap-y-2 py-50">
-            <h1 class="relative text-3xl font-bold text-white">
+            <h1 class="relative text-3xl md:text-5xl font-bold text-amber-500">
                 Welcome to Pawon Sari
             </h1>
-            <h3 class="relative text-lg font-semibold text-white">Your favorite place for delicious cakes and
+            <h3 class="relative text-lg md:text-xl font-semibold text-white">Your favorite place for delicious cakes and
                 pastries.</h3>
         </div>
         {{-- overlay gradient --}}
         <div class="absolute bottom-0 left-0 right-0 h-15 bg-linear-to-t from-white to-transparent z-10"></div>
     </section>
-    <div>
-        Your favorite place for delicious cakes and pastries.
+    <h2 class="text-2xl text-center font-bold mb-10 text-amber-400">Discover the Sweetest Delights at Pawon Sari!</h2>
+    <div class="flex justify-center items-center">
+        <div class="flex flex-col md:flex-row gap-8">
+            <div
+                class="bg-white border-2 border-amber-400 w-72 p-6 rounded-2xl shadow-lg text-center hover:-translate-y-2 hover:shadow-2xl transition duration-300">
+                <h3 class="text-xl font-semibold mb-3">Freshly Baked Goodies</h3>
+                <p class="text-gray-700">Indulge in our wide variety of cakes, pastries, and desserts made with the finest
+                    ingredients.</p>
+            </div>
+            <div
+                class="bg-white border-2 border-amber-400 w-72 p-6 rounded-2xl shadow-lg text-center hover:-translate-y-2 hover:shadow-2xl transition duration-300">
+                <h3 class="text-xl font-semibold mb-3">Special Offers</h3>
+                <p class="text-gray-700">Enjoy exclusive discounts and promotions for our loyal customers.</p>
+            </div>
+        </div>
+        <div>
+
+        </div>
     </div>
     <div>
         Explore our menu and order your favorites today!
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,19 +1,41 @@
     <nav class="bg-white shadow-md sticky top-0 z-50">
         <div class="w-full mx-auto px-5">
             <div class="flex items-center justify-between h-20">
-                <div class="shrink-0 text-xl font-bold">Pawon Sari</div>
-                <div class="flex gap-x-4">
+                <div class="shrink-0 text-xl font-bold text-amber-500">Pawon Sari</div>
+                <div class="hidden md:flex gap-x-4 font-medium">
                     {{-- <a href="#" class="hover:text-orange-500">Home</a> --}}
                     <a href="#" class="hover:text-orange-500">About</a>
                     <a href="#" class="hover:text-orange-500">Menu</a>
                     <a href="#" class="hover:text-orange-500">Contact</a>
                 </div>
-                {{-- <div class="flex items-center space-x-4">
-                    <a href="#" class="hover:text-orange-500">Login</a>
-                    <a href="#" class="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600">
-                        Order
-                    </a>
-                </div> --}}
+                <button id="mob-btn" class="md:hidden text-amber-500 focus:outline-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+        <!-- Mobile Menu -->
+        <div id="mobile-menu" class="max-h-0 overflow-hidden transition-all duration-300 md:hidden px-5 font-medium">
+            <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <a href="#"
+                    class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-orange-500">Home</a>
+                <a href="#"
+                    class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-orange-500">About</a>
+                <a href="#"
+                    class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-orange-500">Menu</a>
+                <a href="#"
+                    class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-orange-500">Contact</a>
             </div>
         </div>
     </nav>
+
+    <script>
+        document.getElementById('mob-btn').addEventListener('click', function() {
+            const menu = document.getElementById('mobile-menu');
+            menu.classList.toggle('max-h-0');
+            menu.classList.toggle('max-h-60');
+        });
+    </script>
